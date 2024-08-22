4b92a8738c01d954ca82f3e36f35da36e617a2bb
Hello-Wolrd-Course <hello.world.course.py@gmail.com>
2024-08-22 11:29:47 +0300

    second leson

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,8 @@ from flask import request, session, redirect, url_for
 from flask import current_app as app, render_template
 
 
-@app.route('/FIXME_WRONG_END_POINT', methods=['FIXME_WRONG_METHODS'])
+@app.route('/game', methods=['GET'])
 def game():
-    if request.method == 'POST':
-        # add logic for words
     return render_template('game.html', words=session.get('used_words', []))
 
 
